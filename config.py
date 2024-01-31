7efc466fad8903474583fe3acffc73c244611ac2
Muiz123 <145835848+muiz12320@users.noreply.github.com>
2024-02-01 02:39:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    API_ID = "25505630
+    API_ID = "25505630"
     API_HASH = "5c4d2a44a1f364ec36cae8a2f451fda9"
     TOKEN = "5846623056:AAHuof1IRXSC4DubI3Ok9jLtgqo3Di5KmrE"  
     START_PIC = "" 
